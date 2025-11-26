67a677d9578b6dd35b4acfb8ac3c44e3b5ff1c0a
Luke <lucasbarabaschihry@gmail.com>
2025-11-26 12:36:10 +0100

    server.js acabado
    Updated video list API to include descriptions and improved comments.

--- justflix/backend/src/server.js
+++ justflix/backend/src/server.js
@@ -10,27 +10,36 @@ app.use(cors({
     allowedHeaders: ['Content-Type'],
     }));
 app.use(express.json());
+app.use('/stream', express.static(path.join(__dirname, 'hls')));
+const streamRoutes = require('../routes/streamRoutes');
+app.use('/hls', streamRoutes);
 
 const VIDEOS_PATH = path.join(__dirname, 'data', 'videos.json');
 
-function readVideos() {
+
+
+function readVideos() { 
     const raw = fs.readFileSync(VIDEOS_PATH, 'utf8');
     return JSON.parse(raw);
 }
+// Leer los videos desde un JSON y devolverlos como objeto
 
-// GET /api/videolist -> lista básica (id, topic, duration, thumbnail)
+
+
+// GET (id, topic, duration, thumbnail)
 app.get('/api/videolist', (req, res) => {
     const all = readVideos();
     const basic = all.map(v => ({
     id: v.id,
     topic: v.topic,
+    description: v.description,
     duration: v.duration,
     thumbnail: v.thumbnail
     }));
     res.json(basic);
 });
 
-// GET /api/videolist/topic/:topic -> lista completa por topic
+// GET lista completa por topic
 app.get('/api/videolist/topic/:topic', (req, res) => {
     const topic = req.params.topic;
     const all = readVideos();
@@ -38,7 +47,7 @@ app.get('/api/videolist/topic/:topic', (req, res) => {
     res.json(filtered);
 });
 
-// GET /api/videolist/id/:id -> objeto completo por id
+// GET objeto completo por id
 app.get('/api/videolist/id/:id', (req, res) => {
     const id = req.params.id;
     const all = readVideos();
